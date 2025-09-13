97d912da1d084807645558b69f18660ad05f5513
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-13 23:19:32 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,14 +1,1 @@
-@echo off
-setlocal
-
-REM Determine the location of the gradle-wrapper.jar
-set WRAPPER_JAR=%~dp0gradle\wrapper\gradle-wrapper.jar
-
-REM Check if gradle-wrapper.jar exists
-if not exist "%WRAPPER_JAR%" (
-    echo Gradle wrapper JAR not found at %WRAPPER_JAR%
-    exit /b 1
-)
-
-REM Execute the Gradle wrapper
-java -jar "%WRAPPER_JAR%" %*
+// placeholder for gradlew.bat
