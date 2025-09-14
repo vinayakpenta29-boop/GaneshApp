adb144bd2f886e28bb16bbff0392ea8d74a5802f
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-15 00:17:02 +0530

    Update Transaction.java

--- app/src/main/java/com/example/expensemanager/Transaction.java
+++ app/src/main/java/com/example/expensemanager/Transaction.java
@@ -1,13 +1,15 @@
 package com.expensemanager;
 
 public class Transaction {
-    public String type, note, date;
+    public String type, note, month, year, date;
     public double amount;
 
-    public Transaction(String type, double amount, String note, String date) {
+    public Transaction(String type, double amount, String note, String month, String year, String date) {
         this.type = type;
         this.amount = amount;
         this.note = note;
+        this.month = month;
+        this.year = year;
         this.date = date;
     }
 }
