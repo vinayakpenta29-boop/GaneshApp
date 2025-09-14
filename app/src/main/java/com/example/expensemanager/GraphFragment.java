8563fd57f0efd14ee998941fe6e16fb295d76cdc
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-14 23:22:59 +0530

    Update GraphFragment.java

--- app/src/main/java/com/example/expensemanager/GraphFragment.java
+++ app/src/main/java/com/example/expensemanager/GraphFragment.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 
@@ -29,6 +31,18 @@ public class GraphFragment extends Fragment {
         BarData data = new BarData(set);
 
         chart.setData(data);
+
+        // Customize X-axis labels for months
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        }));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f);
+        xAxis.setLabelCount(12);
+
+        chart.getDescription().setEnabled(false);
+        chart.animateY(1000);
         chart.invalidate();
 
         return view;
