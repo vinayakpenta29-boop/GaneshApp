f0b4e0b249efe3ad3479d3eb6eb6a0251ee81009
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-16 00:32:23 +0530

    Update SummaryFragment.java

--- app/src/main/java/com/example/expensemanager/SummaryFragment.java
+++ app/src/main/java/com/example/expensemanager/SummaryFragment.java
@@ -9,23 +9,34 @@ import androidx.fragment.app.Fragment;
 
 public class SummaryFragment extends Fragment {
     private DatabaseHelper db;
+    private TextView tvIncome, tvExpenses, tvBalance;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
         db = new DatabaseHelper(getContext());
+        tvIncome = view.findViewById(R.id.tv_income_total);
+        tvExpenses = view.findViewById(R.id.tv_expenses_total);
+        tvBalance = view.findViewById(R.id.tv_balance);
+
+        updateSummary();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateSummary();
+    }
+
+    private void updateSummary() {
         double income = db.getTotalByType("income");
         double expenses = db.getTotalByType("expense");
-
-        TextView tvIncome = view.findViewById(R.id.tv_income_total);
-        TextView tvExpenses = view.findViewById(R.id.tv_expenses_total);
-        TextView tvBalance = view.findViewById(R.id.tv_balance);
+        double balance = income - expenses;
 
         tvIncome.setText("Total Income: " + income);
         tvExpenses.setText("Total Expenses: " + expenses);
-        tvBalance.setText("Balance: " + (income - expenses));
-
-        return view;
+        tvBalance.setText("Balance: " + balance);
     }
 }
