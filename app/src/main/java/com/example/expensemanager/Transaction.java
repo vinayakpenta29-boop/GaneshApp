ebaf4662aa52358dbe46a3f57d8737ce2e454512
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-11-13 23:07:17 +0530

    Add category field to Transaction class

--- app/src/main/java/com/example/expensemanager/Transaction.java
+++ app/src/main/java/com/example/expensemanager/Transaction.java
@@ -1,15 +1,16 @@
 package com.expensemanager;
 
 public class Transaction {
-    public String type, note, month, year, date;
+    public String type, note, month, year, category, date;
     public double amount;
 
-    public Transaction(String type, double amount, String note, String month, String year, String date) {
+    public Transaction(String type, double amount, String note, String month, String year, String category, String date) {
         this.type = type;
         this.amount = amount;
         this.note = note;
         this.month = month;
         this.year = year;
+        this.category = category;
         this.date = date;
     }
 }
